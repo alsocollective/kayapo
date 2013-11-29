a4f27e01a444e78fcb685906689538cfd06e6230
Bohdan <bohdan.anderson@gmail.com>
2013-11-28 20:37:04 -0500

    yep

--- js/main.js
+++ js/main.js
@@ -22,34 +22,69 @@ var GLOBAL = {
 		el1.parentNode.removeChild(el1);
 		el2.parentNode.removeChild(el2);
 	},
+	navHeight:0,
+	getNavHeight:function(){
+		GLOBAL.navHeight = $("nav").outerHeight();
+	},
 	hotSpots:null,
 	hotSpotEl:null,
+	current:null,
+	preScroll:0,
 	getHotSpots:function(){
-		GLOBAL.hotSpotEl = $("section");
+		GLOBAL.hotSpotEl = $(document.body).children();
 		GLOBAL.hotSpots = [];
 		for(var a = 0, max = GLOBAL.hotSpotEl.length; a < max; a += 1){
 			GLOBAL.hotSpots.push($(GLOBAL.hotSpotEl[a]).offset().top);
 		}
+		console.log(GLOBAL.hotSpots);
+		GLOBAL.current = GLOBAL.findClosestElement($(window).scrollTop());
 	},
 	animating:false,
-	scrollEvent:function(){
+	scrollEvent:function(event){
+		event.preventDefault();
 		if(GLOBAL.animating){
 			console.log("already animating");
 			return null;
 		}
 		var loc = $(window).scrollTop();
-		console.log(GLOBAL.findClosestElement(loc));
-		console.log(GLOBAL.hotSpotEl);
-		// $(GLOBAL.hotSpotEl[GLOBAL.findClosestElement(loc)]).scrollView();
+		var newLoc = GLOBAL.findClosestElement(loc);
+
+		if(GLOBAL.current != newLoc && !GLOBAL.animating && newLoc!== null){
+			GLOBAL.animating = true;
+			$('html, body').stop().animate({
+				scrollTop: (GLOBAL.hotSpots[newLoc]-GLOBAL.navHeight)
+			}, 1000,function(){
+				GLOBAL.animating = false;
+			});
+			GLOBAL.current = newLoc;
+		}
+		GLOBAL.preScroll = loc;
 	},
 	findClosestElement:function(loc){
-		for(var a = 0, max = GLOBAL.hotSpots.length; a < max; a += 1){
-			if(loc > 0 && loc > GLOBAL.hotSpots[a] && loc < GLOBAL.hotSpots[a+1]){
-				return a;
-				break;
+		for(var a = 0, max = GLOBAL.hotSpots.length; a < max; ++a){
+			if(loc > GLOBAL.hotSpots[a]-150-GLOBAL.navHeight && loc < GLOBAL.hotSpots[a]-1-GLOBAL.navHeight){
+				if(GLOBAL.preScroll-loc>0){
+					console.log("up");
+					console.log(a-1);
+					return a-1;
+				}
 			}
+			if(loc < GLOBAL.hotSpots[a]+150-GLOBAL.navHeight && loc > GLOBAL.hotSpots[a]-GLOBAL.navHeight){
+				console.log(GLOBAL.preScroll-loc);
+				if(GLOBAL.preScroll-loc<0){
+					console.log("down")
+					console.log(a+1);
+					if(a == 0){
+						return 2;
+					}
+					if(a+1 < max){
+						return a+1;
+					}
+				}
+			}
+
 		}
-		return 0;
+		return null;
 	}
 }
 
@@ -60,18 +95,8 @@ GLOBAL.setUpIndex();
 $(window).scroll(GLOBAL.scrollEvent);
 
 $(window).load(function(){
+	GLOBAL.getNavHeight();
 	GLOBAL.getHotSpots();
 	// GLOBAL.singleIndex();
 
 });
-
-$.fn.scrollView = function () {
-	return this.each(function () {
-		GLOBAL.animating = true;
-		$('html, body').animate({
-			scrollTop: $(this).offset().top
-		}, 1000,function(){
-			GLOBAL.animating = false;
-		});
-	});
-}
