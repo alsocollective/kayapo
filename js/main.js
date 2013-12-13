4d35bfe15ead726615afc2293711aeb239df6946
Bohdan <bohdan.anderson@gmail.com>
2013-12-13 18:29:43 -0500

    on evens an other chnagtes

--- js/main.js
+++ js/main.js
@@ -1,3 +1,20 @@
+/*
+       d8888  .d8888b.  888       .d88888b.         .d8888b.           888 888                   888    d8b
+      d88888 d88P  Y88b 888      d88P" "Y88b       d88P  Y88b          888 888                   888    Y8P
+     d88P888 Y88b.      888      888     888       888    888          888 888                   888
+    d88P 888  "Y888b.   888      888     888       888         .d88b.  888 888  .d88b.   .d8888b 888888 888 888  888  .d88b.
+   d88P  888     "Y88b. 888      888     888       888        d88""88b 888 888 d8P  Y8b d88P"    888    888 888  888 d8P  Y8b
+  d88P   888       "888 888      888     888       888    888 888  888 888 888 88888888 888      888    888 Y88  88P 88888888
+ d8888888888 Y88b  d88P 888      Y88b. .d88P       Y88b  d88P Y88..88P 888 888 Y8b.     Y88b.    Y88b.  888  Y8bd8P  Y8b.
+d88P     888  "Y8888P"  88888888  "Y88888P"         "Y8888P"   "Y88P"  888 888  "Y8888   "Y8888P  "Y888 888   Y88P    "Y8888
+*/
+
+
+
+
+
+
+
 //detect if it's a mobile browser
 (function(a){(jQuery.browser=jQuery.browser||{}).mobile=/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))})(navigator.userAgent||navigator.vendor||window.opera);
 
@@ -10,7 +27,16 @@ if(jQuery.browser.mobile){
 		MOBILE = true;
 	}
 }
-
+/*
+ .d8888b.  888       .d88888b.  888888b.          d8888 888
+d88P  Y88b 888      d88P" "Y88b 888  "88b        d88888 888
+888    888 888      888     888 888  .88P       d88P888 888
+888        888      888     888 8888888K.      d88P 888 888
+888  88888 888      888     888 888  "Y88b    d88P  888 888
+888    888 888      888     888 888    888   d88P   888 888
+Y88b  d88P 888      Y88b. .d88P 888   d88P  d8888888888 888
+ "Y8888P88 88888888  "Y88888P"  8888888P"  d88P     888 88888888
+*/
 var GLOBAL = {
 	debug:false,
 	address: null,
@@ -139,15 +165,12 @@ var GLOBAL = {
 		var loc = $("#content").scrollTop();
 		$("#content").scrollTop(GLOBAL.hotSpots[GLOBAL.current]-GLOBAL.navHeight)
 		var newLoc = 0;
-		console.log("---------------")
-		console.log(loc, GLOBAL.hotSpots[GLOBAL.current]-GLOBAL.navHeight);
 		var tollerence = 30;
 		if(loc > GLOBAL.hotSpots[GLOBAL.current]-GLOBAL.navHeight+tollerence){
 			newLoc = GLOBAL.current + 1;
 		} else if(loc < GLOBAL.hotSpots[GLOBAL.current]-GLOBAL.navHeight-tollerence){
 			newLoc = GLOBAL.current - 1;
 		} else {
-			console.log("is equal");
 			return false;
 		}
 		// var newLoc = GLOBAL.findClosestElement(loc);
@@ -176,7 +199,9 @@ var GLOBAL = {
 		GLOBAL.setHash(GLOBAL.hotSpotEl[location].id);
 	},
 	edgeCases:function(location){
-		if(GLOBAL.debug)console.log("edgeCase " + location);
+		console.log("-----------")
+		/*if(GLOBAL.debug)*/console.log("edgeCase " + location);
+		console.log(GLOBAL.stuckThreatLoc , GLOBAL.stuckWhyLoc , GLOBAL.stuckAboutLoc);
 
 		//unstuck everything except if needed
 		if(GLOBAL.stuckThreat || GLOBAL.stuckWhy || GLOBAL.stuckAbout){
@@ -412,11 +437,13 @@ var GLOBAL = {
 	stuckThreat:false,
 	stuckThreatLoc:null,
 	setWhyStatic:function(){
-		var el = $("#why-rainforest-3 #cropping-youtube").addClass('stick');
+		console.log("why - sticks");
+		$("#why-rainforest-3 #cropping-youtube").addClass('stick');
 		GLOBAL.stuckWhy = true;
 	},
 	unSetWhyStatic:function(){
-		var el = $("#why-rainforest-3 #cropping-youtube").removeClass('stick');
+		console.log("why - un - sticks");
+		$("#why-rainforest-3 #cropping-youtube").removeClass('stick');
 		GLOBAL.stuckWhy = false;
 	},
 	setAboutStatic:function(){
@@ -607,18 +634,27 @@ var GLOBAL = {
 		}
 
 		return false;
+	},
+	hideWhyVideo:function(){
+		$("#cropping-youtube").hide('0');
+	},
+	showWhyVideo:function(){
+		$("#cropping-youtube").show('0');
 	}
-
 }
 
 
 
-
-
-
-
-
-
+/*
+88888888888  .d88888b.  888     888  .d8888b.  888    888
+    888     d88P" "Y88b 888     888 d88P  Y88b 888    888
+    888     888     888 888     888 888    888 888    888
+    888     888     888 888     888 888        8888888888
+    888     888     888 888     888 888        888    888
+    888     888     888 888     888 888    888 888    888
+    888     Y88b. .d88P Y88b. .d88P Y88b  d88P 888    888
+    888      "Y88888P"   "Y88888P"   "Y8888P"  888    888
+*/
 var TOUCH = {
 	scroll:0,
 	active:false,
@@ -654,7 +690,19 @@ var TOUCH = {
 }
 
 
-
+/*
+  888888 8888888b.   .d8888b.   .d8888b.  8888888888  .d88888b.  888     888 8888888888 888b    888  .d8888b.  8888888888
+    "88b 888   Y88b d88P  Y88b d88P  Y88b 888        d88P" "Y88b 888     888 888        8888b   888 d88P  Y88b 888
+     888 888    888 888    888 Y88b.      888        888     888 888     888 888        88888b  888 888    888 888
+     888 888   d88P 888         "Y888b.   8888888    888     888 888     888 8888888    888Y88b 888 888        8888888
+     888 8888888P"  888  88888     "Y88b. 888        888     888 888     888 888        888 Y88b888 888        888
+     888 888        888    888       "888 888        888 Y8b 888 888     888 888        888  Y88888 888    888 888
+     88P 888        Y88b  d88P Y88b  d88P 888        Y88b.Y8b88P Y88b. .d88P 888        888   Y8888 Y88b  d88P 888
+     888 888         "Y8888P88  "Y8888P"  8888888888  "Y888888"   "Y88888P"  8888888888 888    Y888  "Y8888P"  8888888888
+   .d88P                                                    Y8b
+ .d88P"
+888P"
+*/
 var JPGSEQUENCE = {
 	listOfImages:[],
 	listOfButtons:[],
@@ -672,7 +720,7 @@ var JPGSEQUENCE = {
 		buttonContainer = document.createElement("div");
 		buttonContainer.id = "button-container";
 
-		for(var a = 0; a < number; a += 2){
+		for(var a = 1; a < number; a += 2){
 			el = JPGSEQUENCE.createImage(a);
 			JPGSEQUENCE.listOfImages.push(el);
 			parent.appendChild(el);
@@ -753,12 +801,28 @@ var JPGSEQUENCE = {
 	}
 }
 
+
+
+/*
+888       .d88888b.         d8888 8888888b.
+888      d88P" "Y88b       d88888 888  "Y88b
+888      888     888      d88P888 888    888
+888      888     888     d88P 888 888    888
+888      888     888    d88P  888 888    888
+888      888     888   d88P   888 888    888
+888      Y88b. .d88P  d8888888888 888  .d88P
+88888888  "Y88888P"  d88P     888 8888888P"
+*/
 GLOBAL.setUpIndex();
 var YT = null;
 
-
-
 $(window).load(function(){
+	if(MOBILE){
+		GLOBAL.getLocation();
+		setTimeout(GLOBAL.fadeLoading,1000);
+		return false;
+	}
+
 	if($("html.touch")[0]){
 		console.log("this is touch enabled");
 		$(window).on("scroll",TOUCH.preventScroll);
