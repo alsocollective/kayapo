407aa791eae833821b6016c0bb57067e014ff2a0
Bohdan <bohdan.anderson@gmail.com>
2013-12-13 17:25:10 -0500

    fixed the scrolling issue, it's alot more 'sticky' now

--- js/main.js
+++ js/main.js
@@ -14,7 +14,6 @@ if(jQuery.browser.mobile){
 var GLOBAL = {
 	debug:false,
 	address: null,
-	mousePressed:false,
 	getAddress:function(){
 		var temp = window.location.href.toString().split(window.location.host)[1].split(".")[0].split("/")[1];
 		if(!temp) temp = "index-1";
@@ -138,9 +137,22 @@ var GLOBAL = {
 			return false;
 		}
 		var loc = $("#content").scrollTop();
-		var newLoc = GLOBAL.findClosestElement(loc);
-		if(GLOBAL.debug) console.log(newLoc);
-		if(GLOBAL.current != newLoc && !GLOBAL.animating && newLoc!== null){
+		$("#content").scrollTop(GLOBAL.hotSpots[GLOBAL.current]-GLOBAL.navHeight)
+		var newLoc = 0;
+		console.log("---------------")
+		console.log(loc, GLOBAL.hotSpots[GLOBAL.current]-GLOBAL.navHeight);
+		var tollerence = 30;
+		if(loc > GLOBAL.hotSpots[GLOBAL.current]-GLOBAL.navHeight+tollerence){
+			newLoc = GLOBAL.current + 1;
+		} else if(loc < GLOBAL.hotSpots[GLOBAL.current]-GLOBAL.navHeight-tollerence){
+			newLoc = GLOBAL.current - 1;
+		} else {
+			console.log("is equal");
+			return false;
+		}
+		// var newLoc = GLOBAL.findClosestElement(loc);
+		// if(GLOBAL.debug) console.log(newLoc);
+		if(GLOBAL.current != newLoc && newLoc!== null){
 			GLOBAL.highLightNavOnScroll(GLOBAL.hotSpotEl[newLoc].id);
 			GLOBAL.loadPage(newLoc);
 		}
