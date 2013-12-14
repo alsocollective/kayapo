e64e9826773041a6acca79728a4d1b9935922d12
Bohdan <bohdan.anderson@gmail.com>
2013-12-14 00:41:07 -0500

    fixed the us link

--- js/main.js
+++ js/main.js
@@ -603,12 +603,12 @@ var GLOBAL = {
 		});
 	},
 	reDirectTounitedStates:function(){
-		// var mon = $("#donate-donor-mon")[0],
-		// one = $("#donate-donor-one")[0];
-		// if(mon || one){
-		// 	mon.href = "url to canada donnor perfect monthly";
-		// 	one.href = "url to canada donnor perfect onetime";
-		// }
+		var mon = $("#donate-donor-mon")[0],
+		one = $("#donate-donor-one")[0];
+		if(mon || one){
+			mon.href = "https://www.gifttool.com/donations/Donate?ID=1274&AID=2391";
+			one.href = "https://www.gifttool.com/donations/Donate?ID=1274&AID=2391";
+		}
 	},
 	myIP:function() {
 		if (window.XMLHttpRequest) xmlhttp = new XMLHttpRequest();
