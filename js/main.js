4d8f5bd9ad00247d5f1682e83e06e3187902b0fa
symonoliver <symon@alsocollective.com>
2013-12-14 01:23:34 -0500

    moved analytics to head

--- js/main.js
+++ js/main.js
@@ -922,13 +922,3 @@ $(window).load(function(){
 	setTimeout(GLOBAL.fadeLoading,1000);
 });
 
-/* ANALYTICS */
-var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-42487410-2']);
-
-(function() {
-var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-ga.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'stats.g.doubleclick.net/dc.js';
-var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-})();
-
